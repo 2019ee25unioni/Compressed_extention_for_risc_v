adbb3624824be7c964a9639c4c72f270edab0dd9
Abdul Waheed Bhatti <bigbhatti586@gmail.com>
2023-07-14 12:28:24 +0500

    changed a condition for the upper bits storage and updated the structural diagram and fsm state graph

--- c_misalign.sv
+++ c_misalign.sv
@@ -35,7 +35,7 @@ module c_misalign (
     always_ff @( posedge clk ) begin // a register that hold the upper 16 bits of the previous instruction that will be concatinated with the current instruction and sent forwards
         if (reset) upper_16 <=16'b0;
         else begin
-        if (current_state != s0) upper_16 <=upper_16;
+        if (current_state == s1) upper_16 <=upper_16;
         else if (is_missaligned & (current_state == s0)) upper_16<= inst_in[31:16];
         else if ((next_misaligned)& (current_state == s2))upper_16<=conc_32_misallign[31:16];
         end
